e2c6fb63d7e4898db5d419134941b9478fd5b6e5
fish <123904088+fishtested@users.noreply.github.com>
2025-08-06 20:41:33 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-
 const { token } = require('./config.json');
 const { Client, Events, GatewayIntentBits } = require('discord.js');
-
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, async readyClient => {
@@ -18,7 +16,7 @@ async function registerCommands() {
       .setDescription('Gets the current temperature')
       .addStringOption(option =>
         option.setName('id')
-          .setDescription('Enter a location ID')
+          .setDescription('Enter a location')
           .setRequired(true)
       ),
   ].map(cmd => cmd.toJSON());
@@ -39,16 +37,18 @@ client.on(Events.InteractionCreate, async interaction => {
   const { commandName } = interaction;
 
   if (commandName === 'temp') {
-    const id = interaction.options.getString('id'); // the city name
+    const id = interaction.options.getString('id'); // location name
     try {
       const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${id}&units=metric&appid=`); // I removed the API key
       if (!res.ok) throw new Error(`HTTP error ${res.status}`);
       const data = await res.json();
-      const temp = data.main.temp; // stores temperature
-      const city = data.name; // stores city name from API
-      const country = data.sys.country; // stores country
+      const temp = data.main.temp; // temperature
+      const city = data.name; //  location name from API
+      const country = data.sys.country; // country
+      const highTemp = data.main.temp_max; // high temperature
+      const lowTemp = data.main.temp_min; // low temperature
     
-      await interaction.reply(`🌡️ Temperature in ${city}, ${country}: ${temp}°C`);
+      await interaction.reply(`## ${city}, ${country} Weather:\nCurrent Temperature: ${temp}°C\nLow: ${lowTemp}\nHigh: ${highTemp}`);
       
     } catch (error) {
       console.error(error);
